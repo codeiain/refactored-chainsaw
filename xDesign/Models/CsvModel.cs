c9c26da9a825d7323f28be9081e611348373f7a5
codeiain <codeiain@outlook.com>
2021-04-21 16:07:06 +0100

    Added basic test for the CsvService

---
+++ xDesign/Models/CsvModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace xDesign.Models
+{
+    public class CsvModel
+    {
+        public int? RunningNo { get; set; }
+        public string DoBihNumber { get; set; }
+        public string Streetmap { get; set; }
+        public string Geograph { get; set; }
+        public string HillBagging { get; set; }
+        public string Name { get; set; }
+        public int? SmcSection { get; set; }
+        public string RhbSection { get; set; }
+        public string Section { get; set; }
+        public string Heightm { get; set; }
+        public string Heightft { get; set; }
+        public string Map150 { get; set; }
+        public string Map125 { get; set; }
+        public string GridRef { get; set; }
+        public string GridRefXy { get; set; }
+        public int? Xcoord { get; set; }
+        public int? Ycoord { get; set; }
+        public string Num1891 { get; set; }
+        public string Num1921 { get; set; }
+        public string Num1933 { get; set; }
+        public string Num1953 { get; set; }
+        public string Num1969 { get; set; }
+        public string Num1974 { get; set; }
+        public string Num1981 { get; set; }
+        public string Num1984 { get; set; }
+        public string Num1990 { get; set; }
+        public string Num1997 { get; set; }
+        public string Post1997 { get; set; }
+        public string Comments { get; set; }
+    }
+}
